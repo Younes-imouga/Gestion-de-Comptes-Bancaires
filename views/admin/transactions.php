8a5bfeb563e978d1deca83c1322112fc3d52d7b7
samira kibous <kbs130999@gmail.com>
2025-01-09 12:24:20 +0100

    fixing navigation

--- views/admin/transactions.php
+++ views/admin/transactions.php
@@ -32,7 +32,7 @@
 
                 <!-- Navigation -->
                 <nav class="mt-6 flex-grow">
-                        <a href="/" class="flex items-center w-full px-6 py-3 text-white bg-gray-800">
+                        <a href="/dashboardAdmin" class="flex items-center w-full px-6 py-3 text-white bg-gray-800">
                             <i data-lucide="layout-dashboard" class="w-5 h-5 mr-3"></i>
                             <span>Dashboard</span>
                         </a>
